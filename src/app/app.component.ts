d6b62032ae62dba3e2cf3aa097f81e6bc3655446
x001133 <sandeep.sasidharan@klm.com>
2018-10-21 01:19:18 +0200

    components exercise completed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
     <color-picker #colorPicker (color)="colorPreviewer.color = $event" [color]="color"></color-picker>
     <color-previewer #colorPreviewer ></color-previewer>
     
+    <color-sample #sample></color-sample>
+    <input type="text" placeholder="Type your code here"
+    (keyup)="sample.color = input.value" #input >
+    
     <input type="button" (click)="colorPicker.reset()" value="reset" />
   `,
   styleUrls: ['./app.component.css']
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ColorPickerComponent } from './color-picker/color-picker.component';
 import { ColorPreviewerComponent } from './color-previewer/color-previewer.component';
+import { ColorSampleComponent } from './color-sample/color-sample.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ColorPickerComponent,
-    ColorPreviewerComponent
+    ColorPreviewerComponent,
+    ColorSampleComponent
   ],
   imports: [
     BrowserModule
